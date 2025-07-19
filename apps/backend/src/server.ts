f6ac0c2e663e8d2f286333b6cdbc7819d41dac66
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-07-19 14:07:26 +0530

    cors fixed

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", // Be more specific in production, e.g., 'http://localhost:3000'
+        origin: ["http://localhost:3000" , "https://webrtc-shubh.vercel.app"], // Be more specific in production, e.g., 'http://localhost:3000' 
         methods: ["GET", "POST"]
     }
 });
